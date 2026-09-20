64f7a8097bb21daf78dc0d715def81fa08e65d9d
lolchevy <kelseamoore74@gmail.com>
2026-09-20 16:51:30 -0400

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from werkzeug.utils import secure_filename
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='frontend', static_url_path='')
+app = Flask(__name__, static_folder='.', static_url_path='')
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/')
